232bd972ace01f92c9e07ea3ef0fbc50e3c9a028
Mehedi Hasan Joy <mmehedihasanjoyv@gmail.com>
2024-03-22 14:54:35 +0600

    added the equal filtering

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -3,11 +3,12 @@ import { Prisma, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const getAllAdminFromDB = async (params: any) => {
+  const { searchTerm, ...FieldInput } = params;
   const andCondition: Prisma.AdminWhereInput[] = [];
-
+  const adminSearchAbleField = ["name", "email"];
   if (params.searchTerm) {
     andCondition.push({
-      OR: ["name", "email"].map((field) => ({
+      OR: adminSearchAbleField.map((field) => ({
         [field]: {
           contains: params.searchTerm,
           mode: "insensitive",
@@ -15,6 +16,17 @@ const getAllAdminFromDB = async (params: any) => {
       })),
     });
   }
+  console.log(FieldInput);
+  if (Object.keys(FieldInput).length > 0) {
+    andCondition.push({
+      AND: Object.keys(FieldInput).map((key) => ({
+        [key]: {
+          equals: FieldInput[key],
+        },
+      })),
+    });
+  }
+
   const whereCondition: Prisma.AdminWhereInput = { AND: andCondition };
   const result = await prisma.admin.findMany({
     where: whereCondition,
